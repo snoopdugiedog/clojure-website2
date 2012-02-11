1077c8600d7ce47720fb7ff4fd8b6fa8d61cc70e
Scott Dugas <scott.dugas@gmail.com>
2012-02-11 16:44:48 -0500

    Added client-side less
    It might be prudent to switch to server-side less and use a guard
    during development at some point, but for now this works

--- src/website2/views/common.clj
+++ src/website2/views/common.clj
@@ -2,6 +2,12 @@
   (:use [noir.core :only [defpartial]]
         [hiccup.page-helpers :only [include-js include-css html5]]))
 
+(defn include-less [path]
+  [:link {
+          :rel "stylesheet/less"
+          :type "text/css"
+          :href path}])
+
 (defpartial layout [& content]
             (html5
               [:head
@@ -15,7 +21,10 @@
   (html5
    [:head
     [:title "my website2"]
-    (include-js "/js/main.js")]
+    (include-js "/js/main.js")
+    (include-less "/less/basic.less")
+    (include-js "/js/less-1.2.1.min.js")
+    ]
    [:body
     [:div#wrapper
      content]]))
