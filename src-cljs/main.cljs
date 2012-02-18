702a48aebaa9e669451a4092f06e0b367ef34edf
Scott Dugas <scott.dugas@gmail.com>
2012-02-18 14:37:08 -0500

    hook the old add_task func up to the submit button

--- src-cljs/main.cljs
+++ src-cljs/main.cljs
@@ -4,11 +4,21 @@
 
 (defn before [$elem content]
   (.before $elem content))
+(defn submit [$elem func]
+  (.submit $elem func))
 
 (defn ^:export add_task []
   (let [add_task ($ :#add_task)]
     (before add_task (html [:li "New Task"]))
-    ))
+    )
+  false)
 
 (defn ^:export message []
   (js/alert "Hello from clojurescript again"))
+
+
+(defn ^:export startxthj []
+  (submit ($ "#add_task form") add_task))
+
+(set! (.-onload js/window) startxthj)
+
